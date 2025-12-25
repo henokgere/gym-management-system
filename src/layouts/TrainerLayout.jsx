aa5741f66f020baff327234232b4b235c8b73a64
henokgere <henok1234gere@gmail.com>
2025-12-25 20:47:33 +0300

    trainer done

--- src/layouts/TrainerLayout.jsx
+++ src/layouts/TrainerLayout.jsx
@@ -1,9 +1,13 @@
-import TrainersDashboard from "../pages/trainer/TrainerDashboard";
+import Logout from "../components/Logout";
+import TrainerDashboard from "../pages/trainer/TrainerDashboard";
 
 export default function TrainerLayout({ user }) {
   return (
     <div className="trainer-layout">
-      <TrainersDashboard user={user} />
+      <div style={{ maxWidth: 'fit-content', marginLeft: 'auto', marginTop: '10px', marginRight: '10px' }}>
+        <Logout onLogout={() => window.location.reload()} />
+      </div>
+      <TrainerDashboard user={user} />
     </div>
   );
 }
